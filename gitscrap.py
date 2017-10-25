a3dfe71772f60a30ba0b03f4f5b22382079672b4
Subhrajit Prusty <subhrajit1997@gmail.com>
2017-10-25 11:20:18 +0530

    added a json file to keep track of all members modified the script to modify the json file to update with all the contributions

--- gitscrap.py
+++ gitscrap.py
@@ -1,22 +1,41 @@
 from bs4 import BeautifulSoup as bs4
 import requests
 import datetime
+import json
 
-codexstart = "2017-09-15"
-date = str(datetime.date.today())
-username = input("Enter github username : ")
+def contrib(username):
 
-link = "http://www.github.com/"+username+"?tab=overview&from="+codexstart+"&to="+date
+	codexstart = "2017-09-15"
+	date = str(datetime.date.today())
 
-url = requests.get(link)
+	link = "http://www.github.com/"+username+"?tab=overview&from="+codexstart+"&to="+date
 
-soup = bs4(url.text,"html.parser")
+	url = requests.get(link)
 
-tags = soup.findAll("span",{"class":"float-left"})
+	soup = bs4(url.text,"html.parser")
 
-print(username,":")
+	btags = soup.findAll("button")
+	tags = [tag for tag in btags if tag.findChild("span",{"class":"float-left"})]
+	
+	cont = ""
 
-for tag in tags:
-	tagStrLis = str(tag.getText()).split("\n")
-	tagStr = " ".join([x.strip() for x in tagStrLis]).strip()
-	print(tagStr)
+	for tag in tags:
+		tagStrLis = str(tag.getText()).split("\n")
+		tagStr = " ".join([x.strip() for x in tagStrLis]).strip()
+		cont+=tagStr+"\n"
+	
+	return cont
+
+
+# print(contrib("dibyasonu"))
+
+users = open("users.json","r+")
+userstext = users.read()
+j = json.loads(userstext)
+for x in j['members']:
+	y = contrib(x['username'])
+	x['contrib'] = y
+
+
+users.seek(0)
+users.write(str(j).replace("\'",'\"'))
