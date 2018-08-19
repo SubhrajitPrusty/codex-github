614d64ff088a84c3d2115c8a889ac899708aefbd
Subhrajit Prusty <subhrajit1997@gmail.com>
2018-08-20 01:28:56 +0530

    changed to gevent wsgi

--- app.py
+++ app.py
@@ -1,25 +1,34 @@
 from flask import Flask, url_for, render_template
 import json
 import os 
+from gevent.pywsgi import WSGIServer
 
-json_file_path = os.path.join('static', "users.json")
 
-jtext = open(json_file_path,"r")
-j = json.load(jtext)
-members = j['members']
-localtime = j['time']
-context = {"members":members,"localtime":localtime}
+def getContent():
+	json_file_path = os.path.join('static', "users.json")
 
-jtext.close()
+	jtext = open(json_file_path,"r")
+	j = json.load(jtext)
+	members = j['members']
+	localtime = j['time']
+	context = {"members":members,"localtime":localtime}
+	
+	jtext.close()
 
-# print(context)
+	# print(context)
+	return context
+
 
 app = Flask(__name__, static_url_path='/static')
 
+content = getContent()
+
 @app.route("/")
 def hello():
-	return render_template('index.html',context=context)
+	return render_template('index.html',context=content)
 
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 5000))
-	app.run(host='0.0.0.0', port=port)
+	http_server = WSGIServer(('',port),app)
+	http_server.serve_forever()
+	# app.run(host='0.0.0.0', port=port)
