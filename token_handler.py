0b2fb70f84c3a071a83c09086ce667da2acf6a50
Subhrajit Prusty <subhrajit1997@gmail.com>
2018-10-18 01:54:24 +0530

    refactoring and minor changes

--- token_handler.py
+++ token_handler.py
@@ -1,21 +1,16 @@
-
-
-
 import requests
 import json
-#passing the entire response so that it can be used for deletion function as the token is supposed to be temporary
-def create(user_name,password,client_id,client_secret):
-    payload={"client_secret":client_secret, "scopes": ["admin:org", "admin:org_hook", "admin:public_key", "admin:repo_hook", "repo", "user"], "note": "codex-github"}
-    url='https://api.github.com/authorizations/clients/'+client_id
-    response=requests.put(url, data=json.dumps(payload), auth=(user_name, password))
-    return response.json()
 
+def create(username, password, cid, csecret):
+	payload = {
+				"client_secret" : csecret,
+				"scopes": ["admin:org", "admin:org_hook", "admin:public_key", "admin:repo_hook", "repo", "user"],
+				"note": "codex-github"
+				}
+	AUTH_URL = f'https://api.github.com/authorizations/clients/{cid}'
+	response = requests.put(url, data=json.dumps(payload), auth=(username, password))
+	return response.json()
 
-
-# url looks like https://api.github.com/authorizations/340896353
-def delete(user_name,password,url):
-    response=requests.delete(url, auth=(user_name, password))
-    return response.status_code
-
-
-
+def delete(username, password, url):
+	response = requests.delete(url, auth=(username, password))
+	return (response.status_code == 204)
