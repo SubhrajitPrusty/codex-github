9671474ab987dd75e38ea3d665c51a8505b663ec
Dibya ranjan jena <dibyajena917@gmail.com>
2018-10-17 22:21:19 +0530

    added support for creating token under Oauth application as well as deleting the token.

--- token_handler.py
+++ token_handler.py
@@ -4,16 +4,17 @@
 import requests
 
 #passing the entire response so that it can be used for deletion function as the token is supposed to be temporary
-def create(user_name,password):
-    payload='{"scopes": ["admin:org", "admin:org_hook", "admin:public_key", "admin:repo_hook", "repo", "user"], "note": "codex-github"}'
-    response=requests.post('https://api.github.com/authorizations',data=payload,auth=(user_name, password))
+def create(user_name,password,client_id,client_secret):
+    payload='{"client_secret":client_secret, "scopes": ["admin:org", "admin:org_hook", "admin:public_key", "admin:repo_hook", "repo", "user"], "note": "codex-github"}'
+    url='https://api.github.com/authorizations/clients/'+client_id
+    response=requests.put(url, data=payload, auth=(user_name, password))
     return response.json()
 
 
 
-# node link looks like https://api.github.com/authorizations/220896353
-def delete(user_name,password,node_link):
-    response=requests.delete(node_link, auth=(user_name, password))
+# url looks like https://api.github.com/authorizations/340896353
+def delete(user_name,password,url):
+    response=requests.delete(url, auth=(user_name, password))
     return response.status_code
 
 
