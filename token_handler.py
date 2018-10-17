40b01d37ff5a9cef4e362b892a9dc4e43c61a174
Dibya ranjan jena <dibyajena917@gmail.com>
2018-10-17 22:32:16 +0530

    minor fixes. my bad!

--- token_handler.py
+++ token_handler.py
@@ -2,12 +2,12 @@
 
 
 import requests
-
+import json
 #passing the entire response so that it can be used for deletion function as the token is supposed to be temporary
 def create(user_name,password,client_id,client_secret):
-    payload='{"client_secret":client_secret, "scopes": ["admin:org", "admin:org_hook", "admin:public_key", "admin:repo_hook", "repo", "user"], "note": "codex-github"}'
+    payload={"client_secret":client_secret, "scopes": ["admin:org", "admin:org_hook", "admin:public_key", "admin:repo_hook", "repo", "user"], "note": "codex-github"}
     url='https://api.github.com/authorizations/clients/'+client_id
-    response=requests.put(url, data=payload, auth=(user_name, password))
+    response=requests.put(url, data=json.dumps(payload), auth=(user_name, password))
     return response.json()
 
 
