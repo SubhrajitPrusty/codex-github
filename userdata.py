e42f7f2ddb47af29fb057bab2dc8666a424e0624
Subhrajit <subhrajit1997@gmail.com>
2018-10-17 15:06:13 +0530

    added user not found condition

--- userdata.py
+++ userdata.py
@@ -9,104 +9,105 @@ csecret = os.environ['CLIENT_SECRET']
 
 class Member():
 
-    def __init__(self, u):
-        self.username = u
-        self.name = ""
-        self.avatar = ""
-        self.bio = ""
-        self.followers = 0
-        self.following = 0
-        self.REPOS_URL = f"https://api.github.com/users/{u}/repos"
-        self.repos = []
-        self.nRepos = 0
-        self.totalCommits = 0
+	def __init__(self, u):
+		self.username = u
+		self.name = ""
+		self.avatar = ""
+		self.bio = ""
+		self.followers = 0
+		self.following = 0
+		self.REPOS_URL = f"https://api.github.com/users/{u}/repos"
+		self.repos = []
+		self.nRepos = 0
+		self.totalCommits = 0
 
-    def fetch(self):
-        self.getUser()
-        self.getAllCommits()
+	def fetch(self):
+		self.getUser()
+		self.getAllCommits()
 
-    def printData(self):
-        print("Name :", self.name)
-        print("Username :", self.username)
-        print("Avatar :", self.avatar)
-        print("Bio :", self.bio)
-        print("Public Repos :", self.nRepos)
-        print("Followers :", self.followers)
-        print("Following :", self.following)
-        print("Total Commits :", self.totalCommits)
+	def printData(self):
+		print("Name :", self.name)
+		print("Username :", self.username)
+		print("Avatar :", self.avatar)
+		print("Bio :", self.bio)
+		print("Public Repos :", self.nRepos)
+		print("Followers :", self.followers)
+		print("Following :", self.following)
+		print("Total Commits :", self.totalCommits)
 
-    def getUser(self):
-        payload = {
-            "client_id": cid,
-            "client_secret": csecret
-        }
+	def getUser(self):
+		payload = {
+			"client_id": cid,
+			"client_secret": csecret
+		}
 
-        USER_API = "https://api.github.com/users/{}".format(self.username)
-        r = requests.get(USER_API, params=payload)
-        print(r, f"FETCHING {self.username}", USER_API)
-        userdata = json.loads(r.text)
-        try:
-            self.avatar = userdata['avatar_url']
-            self.name = userdata['name']
-            self.REPOS_URL = userdata['repos_url']
-            self.nRepos = userdata['public_repos']
-            self.bio = userdata['bio']
-            self.followers = userdata['followers']
-            self.following = userdata['following']
-        except:
-            pass
+		USER_API = "https://api.github.com/users/{}".format(self.username)
+		r = requests.get(USER_API, params=payload)
+		print(r, f"FETCHING {self.username}", USER_API)
 
-        r = requests.get(self.REPOS_URL, params=payload)
-        # print(r)
-        print(r, f"FETCHING {self.REPOS_URL}")
-        rep = json.loads(r.text)
-        for rs in rep:
-            self.repos.append(rs['name'])
+		if r.status_code == 404:
+			print("USER NOT FOUND. CHECK LIST")
+		else:
+			userdata = json.loads(r.text)
+			self.avatar = userdata['avatar_url']
+			self.name = userdata['name']
+			self.REPOS_URL = userdata['repos_url']
+			self.nRepos = userdata['public_repos']
+			self.bio = userdata['bio']
+			self.followers = userdata['followers']
+			self.following = userdata['following']		
 
-        return self.avatar, self.name, self.REPOS_URL, self.repos, self.nRepos
+			r = requests.get(self.REPOS_URL, params=payload)
+			# print(r)
+			print(r, f"FETCHING {self.REPOS_URL}")
+			rep = json.loads(r.text)
+			for rs in rep:
+				self.repos.append(rs['name'])
 
-    def getRepoData(self, repo):
-        payload = {
-            "client_id": cid,
-            "client_secret": csecret
-        }
+		return self.avatar, self.name, self.REPOS_URL, self.repos, self.nRepos
 
-        STATS_URL = "https://api.github.com/repos/{}/{}/stats/contributors".format(self.username, repo)
+	def getRepoData(self, repo):
+		payload = {
+			"client_id": cid,
+			"client_secret": csecret
+		}
 
-        r = requests.get(STATS_URL, params=payload)
-        if r.status_code == 202:
-            r = requests.get(STATS_URL, params=payload)
-        # print(r)
-        # print(r.text)
+		STATS_URL = "https://api.github.com/repos/{}/{}/stats/contributors".format(self.username, repo)
 
-        if r.status_code == 403:
-            print("RATE LIMITED")
-            sys.exit(1)
+		r = requests.get(STATS_URL, params=payload)
+		if r.status_code == 202:
+			r = requests.get(STATS_URL, params=payload)
+		# print(r)
+		# print(r.text)
 
-        if len(r.text) == 0:
-            return -1
+		if r.status_code == 403:
+			print("RATE LIMITED")
+			sys.exit(1)
 
-        stats = json.loads(r.text)
-        total = [0]
-        for st in stats:
-            if st['author']['login'].lower() == self.username.lower():
-                total.append(int(st['total']))
-                break
+		if len(r.text) == 0:
+			return -1
 
-        if sum(total) != 0:
-            return sum(total)
-        else:
-            return -1
+		stats = json.loads(r.text)
+		total = [0]
+		for st in stats:
+			if st['author']['login'].lower() == self.username.lower():
+				total.append(int(st['total']))
+				break
 
-    def getAllCommits(self):
-        totalCont = [0]
-        for rs in self.repos:
-            c = self.getRepoData(rs)
-            if c != -1:
-                totalCont.append(c)
-        self.totalCommits = sum(totalCont)
+		if sum(total) != 0:
+			return sum(total)
+		else:
+			return -1
 
-        if self.totalCommits != 0:
-            return self.totalCommits
-        else:
-            return -1
+	def getAllCommits(self):
+		totalCont = [0]
+		for rs in self.repos:
+			c = self.getRepoData(rs)
+			if c != -1:
+				totalCont.append(c)
+		self.totalCommits = sum(totalCont)
+
+		if self.totalCommits != 0:
+			return self.totalCommits
+		else:
+			return -1
