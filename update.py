847e8527d91f6cd35b2cada6a3ed5e636477da62
Puneeth K <32433964+punndcoder28@users.noreply.github.com>
2019-10-20 19:13:14 +0530

    Added exception handling and logging (#131)
    Added exception handling and logging

--- update.py
+++ update.py
@@ -10,52 +10,59 @@ from dotenv import load_dotenv
 load_dotenv()
 dburl = os.environ.get("MONGODB_URI")
 
-client = MongoClient(dburl)
+try:
+	client = MongoClient(dburl)
+	db = client.get_default_database()
 
-db = client.get_default_database()
+	members = db.members
 
-members = db.members
+	users_json = os.path.join("static", "users.json")
 
-users_json = os.path.join("static", "users.json")
+	with open(users_json, "r+") as usernames:
+		usernames = json.loads(usernames.read())
+		
+		# insert if not present
+		for u in usernames:
+			if members.count_documents({"username": re.compile(u, re.IGNORECASE)}) == 0:
+				members.insert_one({"username": u})
+				m = Member(u)
+				m.fetch()
+				del m
 
-with open(users_json, "r+") as usernames:
-	usernames = json.loads(usernames.read())
-	
-	# insert if not present
-	for u in usernames:
-		if members.count_documents({"username": re.compile(u, re.IGNORECASE)}) == 0:
-			members.insert_one({"username": u})
+		# update db
+		for u in usernames:
 			m = Member(u)
 			m.fetch()
-			del m
+			# m.printData()
+			ud = {
+				"name": m.name,
+				"username": m.username,
+				"avatar": m.avatar,
+				"bio": m.bio,
+				"nRepos": m.nRepos,
+				"followers": m.followers,
+				"following": m.following,
+				"totalCommits": m.totalCommits
+			}
 
-	# update db
-	for u in usernames:
-		m = Member(u)
-		m.fetch()
-		# m.printData()
-		ud = {
-			"name": m.name,
-			"username": m.username,
-			"avatar": m.avatar,
-			"bio": m.bio,
-			"nRepos": m.nRepos,
-			"followers": m.followers,
-			"following": m.following,
-			"totalCommits": m.totalCommits
-		}
+			if None in ud.values():
+				m.fetch()
 
-		if None in ud.values():
-			m.fetch()
+			members.update_one(
+				{
+					"username": ud["username"]
+				},
+				{
+					"$set": ud
+				},
+				upsert=False)
 
-		members.update_one(
-			{
-				"username": ud["username"]
-			},
-			{
-				"$set": ud
-			},
-			upsert=False)
-
-for mem in members.find():
-	print(mem)
+	for mem in members.find():
+		print(mem)
+except ConnectionError:
+	print("Could not connect to database")
+except Exception as e:
+	if type(e).__name__=='PyMongoError':
+		print("Could not connect to database")
+	else:
+		print("Error: ", e)
